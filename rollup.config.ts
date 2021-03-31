763c7abd623d1c34b5b1b12249e4bebade367742
Khaled FERJANI <kferjani@linagora.com>
2021-03-31 15:01:18 +0100

    added umd support

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
@@ -9,17 +8,21 @@ import pkg from './package.json'
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'es', sourcemap: true },
+    { file: pkg.main, name: 'dav-parser', format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
   ],
   external: [],
   watch: {
     include: 'src/**',
   },
   plugins: [
-    typescript(),
+    typescript({
+      sourceMap: true,
+      declaration: true,
+      tsconfig: './tsconfig.json',
+    }),
     commonjs(),
     resolve(),
-    sourceMaps(),
-    terser()
+    terser(),
   ],
-}
+};
